da0d3dbd476ab3d4a5111e00a4adbc0411ffa6bf
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-01-20 19:42:34 +0300

    added  build +5 files
    hello main.cpp.o  main.cpp.d  hello.cpp.o  hello.cpp.d

---
+++ build/CMakeFiles/hello.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/hello.dir/main.cpp.o: \
+ /home/dmitriy/Documents/reho/-Developer_Basic-_tasks/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/dmitriy/Documents/reho/-Developer_Basic-_tasks/hello.hpp
